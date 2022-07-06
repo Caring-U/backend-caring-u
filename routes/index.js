4e5748b41dd816f2a810f9bc3f168d24b61bffd7
wahyurahmana <tn.wahyurahmana@gmail.com>
2022-07-06 08:09:35 +0800

    init index dn psikolog

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
-const res = require('express/lib/response')
-
 const router = require('express').Router()
+const psiokologRouter = require('./psikologRouter.js')
 
-router.get('/', () => {
+router.get('/', (req, res, next) => {
   res.status(200).json({
     status : true,
     message : 'Hello Caring-U'
   })
 })
+
+router.use('/psiokolog',psiokologRouter)
 
 module.exports = router
