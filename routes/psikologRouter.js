6cc9070cc2796dfae40ddd084526743dc576f603
iyunrozikin26 <nasrunrozikin26@gmail.com>
2022-07-11 16:54:44 +0800

    edit profile fitur

--- routes/psikologRouter.js
+++ routes/psikologRouter.js
@@ -2,10 +2,11 @@ const router = require("express").Router();
 const Controller = require("../controllers/PsikologController.js");
 const { authentication, authorizeOwnerPsikolog, checkFoundProfilePsikolog, checkProfilePsikolog } = require("../middlewares/auth");
 
-router.get("/", authentication, Controller.getAllSchedule);
-router.get("/profile", authentication, Controller.profilePsikolog);
-router.post("/profile", authentication, checkFoundProfilePsikolog, Controller.createProfile);
-router.post("/profile/schedule", authentication, checkProfilePsikolog, Controller.createSchedule);
-router.get("/:ScheduleId", authentication, authorizeOwnerPsikolog, Controller.detailSchedule);
+router.get("/", authentication, Controller.getAllSchedule); 
+router.get("/profile", authentication, Controller.profilePsikolog); 
+router.post("/profile", authentication, checkFoundProfilePsikolog, Controller.createProfile); 
+router.patch("/profile", authentication, Controller.patchProfile); 
+router.post("/profile/schedule", authentication, checkProfilePsikolog, Controller.createSchedule); 
+router.get("/:ScheduleId", authentication, authorizeOwnerPsikolog, Controller.detailSchedule); 
 
 module.exports = router;
